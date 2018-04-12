73cdb7f36f654032106eff031035970955376724
Bert Peters <bert@bertptrs.nl>
2018-04-12 16:58:57 +0200

    Implement asynchronous loading.

--- src/fmri/Drawable.hpp
+++ src/fmri/Drawable.hpp
@@ -5,6 +5,9 @@
 namespace fmri
 {
 
+    /**
+     * Base class for anything to be drawn to the screen.
+     */
     class Drawable
     {
     public:
@@ -12,6 +15,14 @@ namespace fmri
 
         virtual void draw(float time) = 0;
         virtual void patchTransparancy();
+        /**
+         * Do any GL related initialization.
+         *
+         * GL initialization cannot be done in a separate thread, which
+         * is where the constructor might be called.
+         *
+         * The default implementation does nothing.
+         */
         virtual void glLoad();
 
     protected:
